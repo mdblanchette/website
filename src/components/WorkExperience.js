b8c36100a0e8ce1b9076d44cb656eaf477699603
Michael <mdblanchette@ust.hk>
2024-09-13 20:05:52 +0800

    updated resume

--- src/components/WorkExperience.js
+++ src/components/WorkExperience.js
@@ -5,97 +5,101 @@ import AnimatedText from "./AnimatedText";
 import resume from "../../data/resume.json";
 
 const WorkDetails = ({
-  position,
-  company,
-  companyLink,
-  time,
-  address,
-  work,
+    position,
+    company,
+    companyLink,
+    time,
+    address,
+    work,
 }) => {
-  const ref = useRef(null);
-  console.log("work: ", work);
-  return (
-    <li
-      ref={ref}
-      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
-    >
-      <LiIcon reference={ref} />
-      <motion.div
-        initial={{ y: 50 }}
-        whileInView={{ y: 0 }}
-        transition={{ duration: 0.5, type: "spring" }}
-      >
-        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
-          {position}{" "}
-          <a
-            className="capitalize text-primary dark:text-primaryDark"
-            href={companyLink}
-            target={"_blank"}
-          >
-            @{company}
-          </a>
-        </h3>
-        <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
-          {time} | {address}
-        </span>
-        <ul>
-          {work.map((item, index) => (
-            <li
-              key={index}
-              className="text-dark/75 dark:text-light/50 font-medium xs:text-sm"
+    const ref = useRef(null);
+    console.log("work: ", work);
+    return (
+        <li
+            ref={ref}
+            className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]"
+        >
+            <LiIcon reference={ref} />
+            <motion.div
+                initial={{ y: 50 }}
+                whileInView={{ y: 0 }}
+                transition={{ duration: 0.5, type: "spring" }}
             >
-              <span className="mr-2 text-2xl">&#8226;</span> {item}
-            </li>
-          ))}
-        </ul>
-      </motion.div>
-    </li>
-  );
+                <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+                    {position}{" "}
+                    <a
+                        className="capitalize text-primary dark:text-primaryDark"
+                        href={companyLink}
+                        target={"_blank"}
+                    >
+                        @{company}
+                    </a>
+                </h3>
+                <span className="capitalize text-dark/75 font-medium dark:text-light/50 xs:text-sm">
+                    {time} | {address}
+                </span>
+                <ul>
+                    {work.map((item, index) => (
+                        <li
+                            key={index}
+                            className="text-dark/75 dark:text-light/50 font-medium xs:text-sm"
+                        >
+                            <span className="mr-2 text-2xl">&#8226;</span>{" "}
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </motion.div>
+        </li>
+    );
 };
 
 const WorkExperience = () => {
-  const [resumeData, setResumeData] = useState(resume);
+    const [resumeData, setResumeData] = useState(resume);
 
-  useEffect(() => {
-    setResumeData(resume);
-  }, []);
+    useEffect(() => {
+        setResumeData(resume);
+    }, []);
 
-  const ref = useRef(null);
+    const ref = useRef(null);
 
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start end", "center start"],
-  });
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "center start"],
+    });
 
-  return (
-    <div>
-      <AnimatedText
-        text="Work Experience"
-        className="font-bold text-8xl my-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16"
-      />
-
-      <div ref={ref} className="relative w-[75%] mx-auto lg:w-[90%] md:w-full">
-        <motion.div
-          className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark 
-            origin-top  dark:bg-primaryDark dark:shadow-3xl"
-          style={{ scaleY: scrollYProgress }}
-        />
-        <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-          {resumeData["experienceWork"].map((item, index) => (
-            <WorkDetails
-              key={index}
-              position={item.position}
-              company={item.company}
-              companyLink={item.companyLink}
-              time={item.time}
-              address={item.address}
-              work={item.work}
+    return (
+        <div>
+            <AnimatedText
+                text="Work Experience"
+                className="font-bold text-8xl my-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16"
             />
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
+
+            <div
+                ref={ref}
+                className="relative w-[75%] mx-auto lg:w-[90%] md:w-full"
+            >
+                <motion.div
+                    className="absolute left-9 top-0 w-[4px] md:w-[2px] md:left-[30px] xs:left-[20px] h-full bg-dark 
+            origin-top  dark:bg-primaryDark dark:shadow-3xl"
+                    style={{ scaleY: scrollYProgress }}
+                />
+                <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
+                    {resumeData["experienceWork"].map((item, index) => (
+                        <WorkDetails
+                            key={index}
+                            position={item.position}
+                            company={item.company}
+                            companyLink={item.companyLink}
+                            time={item.time}
+                            address={item.address}
+                            work={item.work}
+                        />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
 };
 
 export default WorkExperience;
--- src/pages/experience.js
+++ src/pages/experience.js
@@ -6,41 +6,43 @@ import WorkExperience from "@/components/WorkExperience";
 import ProjectExperience from "@/components/ProjectExperience";
 
 export default function Experience() {
-  return (
-    <>
-      <Head>
-        <title> Experience | Michael Blanchette</title>
-        <meta
-          name="description"
-          content="Learn more about Michael Blanchette's work and project experiences."
-        />
-      </Head>
-      <main
-        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
-      >
-        <Layout className="pt-16">
-          <div className="flex w-full flex-col items-center self-center lg:w-full lg:text-center">
-            <p className="my-4 text-center font-medium md:text-sm sm:!text-xs">
-              Feel free to download the PDF version of my resume below.
-            </p>
-            <div className="my-2 flex items-right lg:self-center">
-              <Link
-                href="/Michael_Blanchette_CV.pdf"
-                target={"_blank"}
-                className={`flex items-center rounded-lg border-2 border-solid p-2.5 px-6 text-lg font-semibold
+    return (
+        <>
+            <Head>
+                <title> Experience | Michael Blanchette</title>
+                <meta
+                    name="description"
+                    content="Learn more about Michael Blanchette's work and project experiences."
+                />
+            </Head>
+            <main
+                className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+            >
+                <Layout className="pt-16">
+                    <div className="flex w-full flex-col items-center self-center lg:w-full lg:text-center">
+                        <p className="my-4 text-center font-medium md:text-sm sm:!text-xs">
+                            Feel free to download the PDF version of my resume
+                            below.
+                        </p>
+                        <div className="my-2 flex items-right lg:self-center">
+                            <Link
+                                href="/Michael_Blanchette_Resume.pdf"
+                                target={"_blank"}
+                                className={`flex items-center rounded-lg border-2 border-solid p-2.5 px-6 text-lg font-semibold
       capitalize hover:border-dark hover:text-dark 
       md:p-2 md:px-4 md:text-base
       bg-gradient-to-r from-blue-700 to-cyan-300 text-white`}
-              >
-                Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
-              </Link>
-            </div>
-          </div>
+                            >
+                                Resume{" "}
+                                <LinkArrow className="ml-1 !w-6 md:!w-4" />
+                            </Link>
+                        </div>
+                    </div>
 
-          <WorkExperience />
-          <ProjectExperience />
-        </Layout>
-      </main>
-    </>
-  );
+                    <WorkExperience />
+                    <ProjectExperience />
+                </Layout>
+            </main>
+        </>
+    );
 }
